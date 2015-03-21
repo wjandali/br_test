643ea685bd856baf63feba5864a007d57d545e9f
Wadud Jandali <wjandali@gmail.com>
2015-03-20 21:50:21 -0700

    console

--- js/application.js
+++ js/application.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
 
-  new App.keyboardView({keyString: 'c,d,e,f,g,a,b', el: '.keyboard'});
+  new App.keyboardView({keyString: 'C,D,E,F,G,A,B', el: '.keyboard'});
   new App.logger({el: '.log'});
 
 
   $('button.play').click(function() {
     App.trigger('play-notes', $('.player-input').val());
-    console.log('should play', $('.player-input').html());
   });
 
 });
